8e4e5bfc8ba08749214ea2e17c56be63c497be72
NataliaGMbtm <natalia.gajda@gmail.com>
2023-07-18 23:15:40 +0200

    dodano druga ikone

---
+++ src/Icons2.js
@@ -0,0 +1,15 @@
+import { FcComments } from "react-icons/fc";
+import React from "react";
+
+class Comments extends React.Component {
+  render() {
+    return (
+      <h3>
+        {" "}
+        Lets go for a <FcComments />?{" "}
+      </h3>
+    );
+  }
+}
+
+export default Comments;
