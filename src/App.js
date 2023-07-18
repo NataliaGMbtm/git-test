660837019cf9f52af17094bef5c33afa7c53523a
NataliaGMbtm <natalia.gajda@gmail.com>
2023-07-18 23:17:22 +0200

    dodano ikone do app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
 import "./App.css";
 import Question from "./Icons";
+import Comments from "./Icons2";
 
 function App() {
   return (
     <div className="App">
       <h1>Pytanie retoryczne:</h1>
       <Question />
+      <Comments />
     </div>
   );
 }
